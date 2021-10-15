6687f65ac7bd4cbc996bccafbe4c7cf148e5fed1
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2021-10-15 14:10:25 +0530

    server For Normal

---
+++ server.js
@@ -0,0 +1,54 @@
+ let express=require('express');
+ let server=express();
+ let port=3000;
+ let data=[ { Name:'Jesus Christ!'},{From:'Najareth!'}]
+
+// --------  For Normal! ------------
+
+//  Only one responce at atime!
+//  server.get('/getdata',(req,res)=>{
+//     //  res.send('Praise the Lord!!!')
+   
+//      console.log(data)
+//      res.send(data)
+//     //  res.json('Praise the Lord!!!')
+//     console.log(req.headers.host)
+
+//  })
+
+
+//  ------------------For Specified Host!---------
+//  server.get('/getdata',(req,res)=>{
+    
+//     console.log(req.headers.host)
+//     if( req.headers.host==='127.0.0.1:3000'){
+//      res.send(data)}
+//     //  res.json('Praise the Lord!!!')
+//     else{
+//         res.json('Praise the Lord!!!')
+//     }
+
+//  })
+
+//  ------------For Specified Browser!-----
+let otharize=(req,res,next)=>{
+    if ( req.headers['user-agent']==='Thunder Client (https://www.thunderclient.io)')
+next()
+    else{
+        res.send('U r unotharized!')
+    }
+}
+
+server.get('/',otharize,(req,res)=>{
+    res.send(data)
+})
+
+
+ server.post('/',(req,res)=>{
+    //  res.send('Prase the Lord!, Hallaluya!!!!')
+     res.send(data)
+     req.headers['user-agent']
+ })
+ server.listen(port,()=>{
+     console.log(`server listening at port no: ${port}`)
+ })
